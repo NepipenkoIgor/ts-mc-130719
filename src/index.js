c99f615d1e564f7713b70177928480c2b4d5db53
sfs-inep <igornepipenko@gmail.com>
2019-07-13 15:10:18 +0300

    Feature: num/str/bool ... [ref:no-ref]

--- src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var isInArray = [1, 2, 3, 4, 5].includes(3);
-console.log(isInArray);
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,28 @@
-// type UserAccount = {
-//     firstName: string,
-//     age: number
-// }
+let num: number = 1;
+let num1: number;
+num1 = 2;
+let num2 = 2;
+num2 = 0xf00d;
+let bool: boolean = true;
+let str: string = 'string';
+let nill: null = null;
+let und: undefined = undefined;
 
-// let userAccount =  {
-//     firstName: 'Igor',
-//     age: 33
-// };
-//
-// let p1: typeof userAccount;
-//
-// class Point {
-//     public x = 1;
-// }
-//
-// const poin1: Point = new Point();
+let bigInt: bigint = 4n;
 
-$('.btn').on('click', () => {
-    console.log('TS is awesome');
-});
+const key1: unique symbol = Symbol('Foo');
+let key2: symbol = Symbol('Bar');
+
+let strictObj = {
+    [key1]: 100,
+    [key2]: 'TS',
+}
+
+let v1 = strictObj[key1];
+
+function fn(_value: typeof key1) {
+
+}
+
+fn(key1);
+fn(key2);
