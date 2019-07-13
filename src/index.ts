f0c419b5814069448f90434f069a019f83d1c6dd
sfs-inep <igornepipenko@gmail.com>
2019-07-13 14:53:32 +0300

    Feature: custom types [ref:no-ref]

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-/// <reference lib="esnext" />
-// import 'core-js/features/array/includes';
+// type UserAccount = {
+//     firstName: string,
+//     age: number
+// }
 
-const isInArray = [1, 2, 3, 4, 5].includes(3);
-console.log(isInArray);
+// let userAccount =  {
+//     firstName: 'Igor',
+//     age: 33
+// };
+//
+// let p1: typeof userAccount;
+//
+// class Point {
+//     public x = 1;
+// }
+//
+// const poin1: Point = new Point();
+
+$('.btn').on('click', () => {
+    console.log('TS is awesome');
+});
